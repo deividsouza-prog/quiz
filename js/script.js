731753d2a0ab2a7985e4551c0079a8e3ad960485
Deivid <dkns1@aluno.ifal.edu.br>
2026-03-10 21:50:04 -0300

    Atualização do projeto, função passar pergunta, função verificar correta, telas de categorias

--- js/script.js
+++ js/script.js
@@ -1,23 +1,55 @@
+//Função troca de pergunta
 const quizBoxes = document.querySelectorAll(".quiz-box");
 const botoesProxima = document.querySelectorAll(".proxima");
 
 let indiceAtual = 0;
 
 botoesProxima.forEach((botao) => {
+  botao.addEventListener("click", () => {
+    if (indiceAtual == quizBoxes.length - 1) {
+
+      botao.innerText = "Finalizar Quiz";
+
+      botao.onclick = () => {
+        window.location.href = "resultadoQuiz.html";
+      };
+
+      return;
+    } else {
+      quizBoxes[indiceAtual].classList.remove("ativo");
+      quizBoxes[indiceAtual].classList.add("inativo");
+
+      indiceAtual++;
+
+      quizBoxes[indiceAtual].classList.remove("inativo");
+      quizBoxes[indiceAtual].classList.add("ativo");
+    }
+  });
+});
+
+
+quizBoxes.forEach((box) => {
+
+  const opcoes = box.querySelectorAll(".opcoes button");
+
+  opcoes.forEach((botao) => {
+
     botao.addEventListener("click", () => {
 
-        // remove ativo da atual
-        quizBoxes[indiceAtual].classList.remove("ativo");
-        quizBoxes[indiceAtual].classList.add("inativo");
+      // desativa apenas as opções da pergunta atual
+      opcoes.forEach((b) => {
+        b.disabled = true;
+      });
 
-        // vai para próxima
-        indiceAtual++;
-
-        // se existir próxima
-        if (indiceAtual < quizBoxes.length) {
-            quizBoxes[indiceAtual].classList.remove("inativo");
-            quizBoxes[indiceAtual].classList.add("ativo");
-        }
+      // verifica se é correta
+      if (botao.dataset.correta) {
+        botao.classList.add("correta");
+      } else {
+        botao.classList.add("errada");
+      }
 
     });
+
+  });
+
 });
